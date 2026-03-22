8634331408a86f890f8dbfa802dde01a63d37303
Riccardo-Maffei <126154411+Riccardo-Maffei@users.noreply.github.com>
2026-03-22 16:26:06 +0100

    Implemented finish track and game conclusion

---
+++ Assets/Track/Finish Line/Scripts/FinishLineBehaviour.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+using utils;
+using Player.Scripts;
+
+
+namespace Track.Finish_Line.Scripts
+{
+    public class FinishLineBehaviour : MonoBehaviour, IInteractive
+    {
+        public void OnCollision(GameObject _) {}
+
+        public void OnTrigger(GameObject player) {
+            GameData.CrossedFinishLine.SetValue(true);
+
+            var playerBehaviour = player.GetComponent<PlayerBehaviour>();
+            if (playerBehaviour != null) playerBehaviour.maxPlayerSpeed = 0;
+
+        }
+    }
+}
--- Assets/Track/Scripts/TrackPieceBehaviour.cs
+++ Assets/Track/Scripts/TrackPieceBehaviour.cs
@@ -1,30 +1,41 @@
 using UnityEngine;
 using Random = UnityEngine.Random;
 
+using utils;
+
 
 namespace Track.Scripts
 {
     public class TrackPieceBehaviour : MonoBehaviour
     {
         public GameObject[] trackObstacleContainers;
+        public GameObject[] trackItemContainers;
+
+        public GameObject finishLinePrefab;
 
         private enum TrackType
         {
             Clear,
             Single,
-            Double
+            Double,
+            Finish
         }
 
         private readonly TrackType[] _trackTypes =  { TrackType.Clear, TrackType.Single, TrackType.Double };
 
-        private void Start()
+        public void GenerateObstacles(float distanceFromStart)
         {
-            GenerateObstacles();
-        }
+            TrackType trackType;
 
-        public void GenerateObstacles()
-        {
-            switch (_trackTypes[Random.Range(0, _trackTypes.Length)])
+            if (distanceFromStart >= GameData.TrackLength.GetValue() && !GameData.SpawnedFinishLine.GetValue())
+            {
+                trackType = TrackType.Finish;
+                GameData.SpawnedFinishLine.SetValue(true);
+            }
+            else if (distanceFromStart >= GameData.TrackLength.GetValue()) trackType = TrackType.Clear;
+            else trackType = _trackTypes[Random.Range(0, _trackTypes.Length)];
+
+            switch (trackType)
             {
                 case TrackType.Single:
                 {
@@ -44,6 +55,20 @@ namespace Track.Scripts
                     for (var obstacleIndex = 0; obstacleIndex < trackObstacleContainers.Length; obstacleIndex++)
                     {
                         trackObstacleContainers[obstacleIndex].SetActive(obstacleIndex != targetIndex);
+                    }
+
+                    break;
+                }
+                case TrackType.Finish:
+                {
+                    foreach (var obstacleContainer in trackObstacleContainers)
+                    {
+                        obstacleContainer.SetActive(false);
+                    }
+
+                    foreach (var trackItemContainer in trackItemContainers)
+                    {
+                        Instantiate(finishLinePrefab, trackItemContainer.transform);
                     }
 
                     break;
