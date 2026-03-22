6039300f1ea6c6ceae08f7770c3a7c935f4fea5b
Alex Sivapalan <sivapale@students.zhaw.ch>
2026-03-22 17:20:59 +0100

    Add implementation to end the game, if there's no more live.

--- Assets/Player/Scripts/PlayerBehaviour.cs
+++ Assets/Player/Scripts/PlayerBehaviour.cs
@@ -1,7 +1,6 @@
 using System;
 using UnityEngine;
 using UnityEngine.InputSystem;
-
 using utils;
 
 namespace Player.Scripts
@@ -10,29 +9,31 @@ namespace Player.Scripts
     {
         private InputAction _moveAction;
 
+        [Header("Physics & Components")]
         public Rigidbody2D playerRigidbody;
 
+        [Header("Lane Settings")]
         public float laneHeight = 1;
         public int laneCount = 3;
+        public float laneSpeed = 10;
+        public float minPlayerY = 0.5f;
+        private float _maxPlayerY;
+        private float _currentPlayerY;
 
+        [Header("Movement Settings")]
         public float minPlayerSpeed = 1;
         public float maxPlayerSpeed = 10;
         public float playerAcceleration = 5;
         private float _currentPlayerSpeed;
-
-        public float minPlayerY = 0.5f;
-        private float _maxPlayerY;
-        public float laneSpeed = 10;
-        private float _currentPlayerY;
-
         private float _oldX;
 
+        // Variable für den Trefferschutz (Cooldown)
+        private bool _canTakeDamage = true;
 
         private void Start()
         {
             _currentPlayerSpeed = minPlayerSpeed;
             _currentPlayerY = minPlayerY;
-
             _maxPlayerY = minPlayerY + laneHeight * laneCount;
 
             _moveAction = InputSystem.actions.FindAction("Move");
@@ -43,17 +44,19 @@ namespace Player.Scripts
 
         private void Update()
         {
+            // Distanz-Tracking für GameData
             GameData.TotalDistance.Increase(Math.Abs(playerRigidbody.position.x - _oldX));
             _oldX = playerRigidbody.position.x;
 
+            // Beschleunigung berechnen
             _currentPlayerSpeed = Mathf.MoveTowards(
                 _currentPlayerSpeed,
                 maxPlayerSpeed,
                 playerAcceleration * Time.deltaTime
             );
 
+            // Position aktualisieren
             var pos = playerRigidbody.position;
-
             pos.x += _currentPlayerSpeed * Time.deltaTime;
             pos.y = Mathf.MoveTowards(pos.y, _currentPlayerY, laneSpeed * Time.deltaTime);
 
@@ -69,21 +72,62 @@ namespace Player.Scripts
         private void OnMovementTrigger(InputAction.CallbackContext ctx)
         {
             var moveValue = ctx.ReadValue<Vector2>();
-
             var newY = _currentPlayerY + moveValue.y * laneHeight;
 
             if (newY >= minPlayerY && newY < _maxPlayerY)
                 _currentPlayerY = newY;
         }
 
-        private void OnCollisionEnter2D(Collision2D other)
+        private void OnCollisionEnter2D(Collision2D collision)
         {
-            other.gameObject.GetComponent<IInteractive>()?.OnCollision(gameObject);
+            // 1. Prüfen, ob wir gerade Schaden nehmen dürfen (verhindert Doppel-Abzug)
+            if (!_canTakeDamage) return;
+
+            // 2. Prüfen, ob das getroffene Objekt ein Hindernis ist
+            if (collision.gameObject.CompareTag("Obstacle"))
+            {
+                _canTakeDamage = false; // Schaden kurzzeitig sperren
+
+                if (GameData.Hp.GetValue() > 0)
+                {
+                    GameData.Hp.Decrease(1);
+                    Debug.Log("Leben abgezogen! Verbleibend: " + GameData.Hp.GetValue());
+
+                    // Nach 0.5 Sekunden wieder Schaden erlauben
+                    Invoke(nameof(ResetDamage), 0.5f);
+                }
+
+                // 3. Prüfen, ob das Spiel beendet werden muss
+                if (GameData.Hp.GetValue() <= 0)
+                {
+                    EndGame();
+                }
+            }
+
+            // Bestehende Interaktions-Logik aufrufen
+            collision.gameObject.GetComponent<IInteractive>()?.OnCollision(gameObject);
         }
 
         private void OnTriggerEnter2D(Collider2D other)
         {
             other.gameObject.GetComponent<IInteractive>()?.OnTrigger(gameObject);
+        }
+
+        // Hilfsmethode, um den Trefferschutz aufzuheben
+        private void ResetDamage()
+        {
+            _canTakeDamage = true;
+        }
+
+        private void EndGame()
+        {
+            Debug.Log("GAME OVER - Lost all lives.");
+
+        #if UNITY_EDITOR
+            UnityEditor.EditorApplication.isPlaying = false;
+        #else
+            Application.Quit();
+        #endif
         }
     }
 }
