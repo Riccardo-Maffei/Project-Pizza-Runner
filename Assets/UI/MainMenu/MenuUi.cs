d569625c5c91b9248830d1b52a83e66773f555b9
Alex Sivapalan <sivapale@students.zhaw.ch>
2026-05-19 03:25:59 +0200

    Add menuUI to endless mode

--- Assets/UI/MainMenu/MenuUi.cs
+++ Assets/UI/MainMenu/MenuUi.cs
@@ -7,7 +7,6 @@ using UnityEngine.InputSystem;
 
 using Utils;
 
-
 namespace UI.MainMenu
 {
     public class MenuUI : MonoBehaviour
@@ -17,8 +16,13 @@ namespace UI.MainMenu
 
         private Button _openTutorialButton;
         private Button _closeTutorialButton;
+        
+        private Button _storyModeButton;
+        private Button _endlessModeButton;
+        private Button _closeModeSelectButton;
 
         private readonly Observable<bool> _tutorialOpen = new (false);
+        private readonly Observable<bool> _modeSelectOpen = new (false);
 
         private Label _distanceField;
         private Label _hpField;
@@ -30,6 +34,7 @@ namespace UI.MainMenu
 
         private VisualElement _mainMenuContainer;
         private VisualElement _tutorialContainer;
+        private VisualElement _modeSelectContainer;
 
         private Action<double> _distanceObserver;
         private Action<int> _hpObserver;
@@ -44,34 +49,93 @@ namespace UI.MainMenu
             
             _mainMenuContainer = root.Q<VisualElement>("MainMenuContainer");
             _tutorialContainer = root.Q<VisualElement>("TutorialContainer");
+            _modeSelectContainer = root.Q<VisualElement>("ModeSelectContainer"); // NEU
             
             _startGameButton = root.Q<Button>("StartGameButton");
             _exitGameButton = root.Q<Button>("ExitGameButton");
                 
             _openTutorialButton = root.Q<Button>("OpenTutorialButton");
             _closeTutorialButton = root.Q<Button>("CloseTutorialButton");
-
-            _startGameButton.clicked += GameHandler.LoadGameScene;
+            
+            _storyModeButton = root.Q<Button>("StoryModeButton");
+            _endlessModeButton = root.Q<Button>("EndlessModeButton");
+            _closeModeSelectButton = root.Q<Button>("CloseModeSelectButton");
+            
+            _startGameButton.clicked += () => _modeSelectOpen.SetValue(true);
             _exitGameButton.clicked += GameHandler.EndGame;
 
             _openTutorialButton.clicked += () => _tutorialOpen.SetValue(true);
             _closeTutorialButton.clicked += () => _tutorialOpen.SetValue(false);
             
+            _closeModeSelectButton.clicked += () => _modeSelectOpen.SetValue(false);
+            _storyModeButton.clicked += OnStoryModeSelected;
+            _endlessModeButton.clicked += OnEndlessModeSelected;
+            
             _tutorialOpen.Subscribe(tutorialOpen => {
                 _mainMenuContainer.style.display = tutorialOpen ? DisplayStyle.None : DisplayStyle.Flex;
                 _tutorialContainer.style.display = tutorialOpen ? DisplayStyle.Flex : DisplayStyle.None;
             });
+            
+            _modeSelectOpen.Subscribe(modeSelectOpen => {
+                _mainMenuContainer.style.display = modeSelectOpen ? DisplayStyle.None : DisplayStyle.Flex;
+                _modeSelectContainer.style.display = modeSelectOpen ? DisplayStyle.Flex : DisplayStyle.None;
+            });
+        }
+        
+        private void OnStoryModeSelected()
+        {
+            PlayerPrefs.SetInt("IsEndlessMode", 0); // 0 = Feste Levels
+            GameHandler.LoadGameScene();
+        }
+
+        private void OnEndlessModeSelected()
+        {
+            PlayerPrefs.SetInt("IsEndlessMode", 1); // 1 = Unendliches Level
+            GameHandler.LoadGameScene();
         }
 
         protected void OnStartGameShortcut(InputValue value)
         {
-            if (!_tutorialOpen.GetValue()) StartCoroutine(LoadGameNextFrame());
+            if (!_tutorialOpen.GetValue() && !_modeSelectOpen.GetValue()) 
+            {
+                _modeSelectOpen.SetValue(true);
+            }
         }
 
         protected void OnExitGameShortcut(InputValue value)
         {
-            if (!_tutorialOpen.GetValue()) StartCoroutine(ExitGameNextFrame());
-            else _tutorialOpen.SetValue(false);
+            // esc to go back
+            if (_modeSelectOpen.GetValue())
+            {
+                _modeSelectOpen.SetValue(false);
+            }
+            // esc to close tutorial
+            else if (_tutorialOpen.GetValue())
+            {
+                _tutorialOpen.SetValue(false);
+            }
+            // esc to close game
+            else 
+            {
+                StartCoroutine(ExitGameNextFrame());
+            }
+        }
+        
+        private void Update()
+        {
+            if (_modeSelectOpen.GetValue() && Keyboard.current != null)
+            {
+                // Taste 1 = Story Modus
+                if (Keyboard.current.digit1Key.wasPressedThisFrame || Keyboard.current.numpad1Key.wasPressedThisFrame)
+                {
+                    OnStoryModeSelected();
+                }
+                // Taste 2 = Endless Modus
+                else if (Keyboard.current.digit2Key.wasPressedThisFrame || Keyboard.current.numpad2Key.wasPressedThisFrame)
+                {
+                    OnEndlessModeSelected();
+                }
+            }
         }
 
         private static IEnumerator LoadGameNextFrame()
@@ -88,8 +152,9 @@ namespace UI.MainMenu
 
         private void OnDisable()
         {
-            _startGameButton.clicked -= GameHandler.LoadGameScene;
             _exitGameButton.clicked -= GameHandler.EndGame;
+            if (_storyModeButton != null) _storyModeButton.clicked -= OnStoryModeSelected;
+            if (_endlessModeButton != null) _endlessModeButton.clicked -= OnEndlessModeSelected;
         }
     }
 }
